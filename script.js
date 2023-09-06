4eb6fae07532cbed9b144091248c6fb8ad582897
shaik-muneeruddin <skmuneeruddin53@gmail.com>
2023-09-06 19:01:53 +0530

    nav bar bug-fix

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ AOS.init({
 
 const menu = document.querySelector('.menu-btn');
 const page = document.getElementById('page-container');
+const navUl = document.getElementById('nav-links')
 const navLinks = document.querySelectorAll('.nav-link');
-const navbar = document.getElementById("navbar");
-const sticky = navbar.offsetTop;
+const navBar = document.getElementById("navbar");
+const sticky = navBar.offsetTop;
 
 
 //javascript to navigate on home section on reload
@@ -21,16 +22,25 @@ window.onload = function () {
 
 
 // js to toggle hamburger menu, add blur effect to the page and to disable scroll when menu is opened
+
+function toggleCheck() {
+    const isOpen = menu.classList.contains('open');
+    navUl.style.display = isOpen ? 'flex' : '';
+    navUl.classList.toggle('resp-nav', isOpen);
+    page.classList.toggle('blur', isOpen);
+    document.body.style.overflow = isOpen ? 'hidden' : '';
+}
+
 function toggleMenu() {
     menu.classList.toggle('open');
-    page.classList.toggle('blur');
-    document.body.style.overflow = menu.classList.contains('open') ? 'hidden' : '';
+    toggleCheck();
 }
 
 // js to set active state to the nav-link
 function clickSingleA(a) {
     navLinks.forEach(link => link.classList.remove('active'));
-    toggleMenu();
+    menu.classList.remove('open');
+    toggleCheck();
     a.classList.add('active');
 }
 
@@ -42,10 +52,10 @@ menu.addEventListener('click', toggleMenu);
 // js Function to stick the nav bar 
 function myFunction() {
     if (window.pageYOffset > sticky) {
-        navbar.classList.add("sticky")
+        navBar.classList.add("sticky")
     }
     else {
-        navbar.classList.remove("sticky");
+        navBar.classList.remove("sticky");
     }
 }
 
@@ -54,8 +64,6 @@ myFunction();
 
 // Attach the onscroll event to the window
 window.onscroll = function () { myFunction() };
-
-
 
 
 
